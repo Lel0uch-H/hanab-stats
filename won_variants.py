5588ba7e269e26b82deda500608a07eb18d06696
Lel0uch <Lel0uch@nowhere.com>
2021-05-05 12:46:01 +0530

    Modified won_variant.py to sort data by number of variants

--- won_variants.py
+++ won_variants.py
@@ -1,11 +1,18 @@
 from sys import argv
+from util import sortBySecond, convertListOfListToCsv
 
 players = open(argv[1]).read().split('\n')
 
-output = open('output/won_variants.csv','w')
+
+output_data = []
 
 for player in players:
-    won_count=open('output/variant_wise_first_win/variant_wise_first_win_%s.csv'%player, encoding='utf-8').read().count('\n')+1
-    output.write('%s,%i\n'%(player,won_count))
+    won_vars=open('output/variant_wise_first_win/variant_wise_first_win_%s.csv'%player, encoding='utf-8').read().split('\n')
+    won_vars = [i.split(',') for i in won_vars if i!='']
+    output_data.append((player,len(won_vars)))
 
+output_data = sortBySecond(output_data)
+
+output = open('output/won_variants.csv','w')
+output.write(convertListOfListToCsv(output_data))
 output.close()
