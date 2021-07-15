f164772a5cf7a73e3ebce5b0725668c9efa697a5
Lel0uch <Lel0uch@nowhere.com>
2021-07-15 18:54:47 +0530

    Added more pairwise statistics

---
+++ pairwise_statistics_kp_all.py
@@ -0,0 +1,23 @@
+from sys import argv
+from os import system
+
+pairs = open(argv[1]).read().split('\n')
+k = argv[2]
+
+filters = [
+    'doubledark',
+    '6suits',
+    '5suits',
+    '6suits nondark',
+    '5suits nondark',
+    '6suits singledark',
+    '5suits singledark',
+    '4suits',
+    '3suits',
+    ''
+]
+
+for pair in pairs:
+    player1,player2 = pair.split(',')
+    for f in filters:
+        system('python3 pairwise_statistics.py %s %s %sp %s'%(player1,player2,k,f))
